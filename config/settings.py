142fb723093eff52aad8dae92de4f8deb4c33e53
yujeong <010308yj@naver.com>
2021-10-27 14:32:04 +0900

    add

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-lu_)qvyyahbapa7l17a16(sk2o*hhjrjepw8xu4v6y!lfa1f_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
